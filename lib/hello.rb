25f3becf0928f9a48ec3a176ef1c4c3e380f6156
Toni-Ty <antonique2479@yahoo.com>
2019-01-12 07:30:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,3 +13,18 @@ end
 #     puts "Hi, #{name}"
 #   end
 # end
+
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
